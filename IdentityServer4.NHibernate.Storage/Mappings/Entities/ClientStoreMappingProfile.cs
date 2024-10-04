73544ea792896f7d099983ff7ad9ea60556ded69
Alexander Lybenko <alexander.ly@askimo.ru>
2024-10-04 17:33:22 +0600

    Added Automapper version limit for storage Added  net3.1 and net6.0 multitargetting Automapper upgrade for net6.0 target

--- IdentityServer4.NHibernate.Storage/Mappings/Entities/ClientStoreMappingProfile.cs
+++ IdentityServer4.NHibernate.Storage/Mappings/Entities/ClientStoreMappingProfile.cs
@@ -26,22 +26,38 @@ namespace IdentityServer4.NHibernate.Mappings.Entities
                         opt => opt.ConvertUsing(AllowedSigningAlgorithmsConverter.Instance, src => src.AllowedIdentityTokenSigningAlgorithms));
 
             CreateMap<NHibernate.Entities.ClientGrantType, string>()
-                .ConstructUsing(src => src.GrantType)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.GrantType) 
+#else
+                 .ConstructUsing(src => src.GrantType)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.GrantType, opt => opt.MapFrom(src => src));
 
             CreateMap<NHibernate.Entities.ClientRedirectUri, string>()
-                .ConstructUsing(src => src.RedirectUri)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.RedirectUri) 
+#else
+                 .ConstructUsing(src => src.RedirectUri)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.RedirectUri, opt => opt.MapFrom(src => src));
 
             CreateMap<NHibernate.Entities.ClientPostLogoutRedirectUri, string>()
-                .ConstructUsing(src => src.PostLogoutRedirectUri)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.PostLogoutRedirectUri) 
+#else
+                 .ConstructUsing(src => src.PostLogoutRedirectUri)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.PostLogoutRedirectUri, opt => opt.MapFrom(src => src));
 
             CreateMap<NHibernate.Entities.ClientScope, string>()
-                .ConstructUsing(src => src.Scope)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.Scope) 
+#else
+                 .ConstructUsing(src => src.Scope)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.Scope, opt => opt.MapFrom(src => src));
 
@@ -50,16 +66,28 @@ namespace IdentityServer4.NHibernate.Mappings.Entities
                 .ReverseMap();
 
             CreateMap<NHibernate.Entities.ClientIdPRestriction, string>()
-                .ConstructUsing(src => src.Provider)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.Provider) 
+#else
+                 .ConstructUsing(src => src.Provider)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.Provider, opt => opt.MapFrom(src => src));
 
             CreateMap<NHibernate.Entities.ClientClaim, ClientClaim>(MemberList.None)
-                .ConstructUsing(src => new ClientClaim(src.Type, src.Value, ClaimValueTypes.String))
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => new ClientClaim(src.Type, src.Value, ClaimValueTypes.String)) 
+#else
+                 .ConstructUsing(src => new ClientClaim(src.Type, src.Value, ClaimValueTypes.String))
+#endif
                 .ReverseMap();
 
             CreateMap<NHibernate.Entities.ClientCorsOrigin, string>()
-                .ConstructUsing(src => src.Origin)
+#if NET6_0_OR_GREATER
+                 .ConstructUsing(src => src.Origin) 
+#else
+                 .ConstructUsing(src => src.Origin)
+#endif
                 .ReverseMap()
                     .ForMember(dst => dst.Origin, opt => opt.MapFrom(src => src));
         }
